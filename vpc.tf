23a3b514daab3123e89d136683a45cd99585cbce
RGaldamez <ricardo.galdamez@gmail.com>
2020-05-16 17:34:21 -0600

    Adding table route associations for public subnets

--- vpc.tf
+++ vpc.tf
@@ -96,3 +96,22 @@ resource "aws_route_table" "main-public" {
       Name = "main-public-1"
   }
 }
+
+#route associations public 
+resource "aws_route_table_association" "main-public-1-a" {
+    subnet_id = "${aws_subnet.main-public-1.id}"
+    route_table_id = "${aws_route_table.main-public.id}"
+}
+
+resource "aws_route_table_association" "main-public-2-a" {
+    subnet_id = "${aws_subnet.main-public-2.id}"
+    route_table_id = "${aws_route_table.main-public.id}"
+}
+
+resource "aws_route_table_association" "main-public-3-a" {
+    subnet_id = "${aws_subnet.main-public-3.id}"
+    route_table_id = "${aws_route_table.main-public.id}"
+  
+}
+
+
